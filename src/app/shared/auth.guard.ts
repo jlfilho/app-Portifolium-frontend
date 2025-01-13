36e9336b725e053b1dec94a49a788c59914741bf
João da Mata <jlfilho@users.noreply.github.com>
2025-01-13 18:42:21 -0400

    Atualização no Guard

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -9,10 +9,10 @@ export const authGuard: CanActivateFn = (route, state) => {
   const apiService = inject(ApiService); // Injeta o serviço de autenticação
   const router = inject(Router); // Injeta o roteador
 
-  if (apiService.isLoggedIn()) {
+  if (apiService.isTokenValid()) {
     return true; // Permite o acesso à rota
   } else {
-    router.navigate(['/login'], { queryParams: { returnUrl: state.url } }); // Redireciona para login
-    return false;
+    apiService.logout(); // Realiza logout
+    return router.createUrlTree(['/login']);
   }
 };
